3af8693ce0e6b3a158c6ed6d85575142781366e3
Siham <siham.charef@etu.unilim.fr>
2023-10-19 01:19:13 +0200

    Indentation

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <!-- Définition de l'encodage de caractères utilisés -->
-    <meta charset="utf-8">
-    <!-- Titre de la page -->
-    <title>Time</title>
-    <!-- Importation de la police Aldrich depuis Google Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aldrich">
-    <!-- Importation du fichier CSS pour le style -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- Importation du fichier Javascript pour le fonctionnement de la page -->
-    <script src="assets/js/script.js"></script>
-</head>
-<body>
-<!-- Conteneur principal de la page -->
-<div id="wrapper">
-    <!-- Conteneur interne -->
-    <div class="inside" id="wrapper">
-        <!-- Affichage de l'heure actuelle -->
-        <div class="timer">
-            <!-- Affichage du nombre d'heures -->
-            <span id="hours">00</span>
-            <!-- Affichage du nombre de minutes -->
-            <span id="minutes">00</span>
-            <!-- Affichage du nombre de secondes -->
-            <span id="seconds">00</span>
+    <head>
+        <!-- Définition de l'encodage de caractères utilisés -->
+        <meta charset="utf-8">
+        <!-- Titre de la page -->
+        <title>Time</title>
+        <!-- Importation de la police Aldrich depuis Google Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aldrich">
+        <!-- Importation du fichier CSS pour le style -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!-- Importation du fichier Javascript pour le fonctionnement de la page -->
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <!-- Conteneur principal de la page -->
+        <div id="wrapper">
+            <!-- Conteneur interne -->
+            <div class="inside" id="wrapper">
+                <!-- Affichage de l'heure actuelle -->
+                <div class="timer">
+                    <!-- Affichage du nombre d'heures -->
+                    <span id="hours">00</span>
+                    <!-- Affichage du nombre de minutes -->
+                    <span id="minutes">00</span>
+                    <!-- Affichage du nombre de secondes -->
+                    <span id="seconds">00</span>
+                </div>
+                <!-- Conteneur des contrôles -->
+                <div class="controls">
+                    <!-- Bouton de play/pause -->
+                    <button class="button pause"></button>
+                </div>
+            </div>
         </div>
-        <!-- Conteneur des contrôles -->
-        <div class="controls">
-            <!-- Bouton de play/pause -->
-            <button class="button pause"></button>
-        </div>
-    </div>
-</div>
-</body>
+    </body>
 </html>
